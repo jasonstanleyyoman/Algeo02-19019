0f671b31626a094b6ce1dec7bc836bd2f32e9dd9
cyn-rus <cyn.rusadi@gmail.com>
2020-11-10 02:55:29 +0700

    vectorize, not perfect yet(?)

---
+++ server/information_retrieval_copy.py
@@ -0,0 +1,196 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pandas as pd
+import numpy as np
+import json
+import os
+import pdftotext
+from bs4 import BeautifulSoup
+
+from preprocess import process_sentence, preprocess
+from get_15_word import get_first_15_words
+from vectorize import vectorize
+
+baseURL = "http://127.0.0.1:5000/file/"
+
+def load_links () :
+    with open ("web-scrapping/links.json") as file :
+        all_links = json.load(file)
+        return all_links
+def load_titles () :
+    with open ("web-scrapping/titles.json") as file :
+        all_titles = json.load(file)
+        return all_titles
+
+def load_data () :
+    with open ("web-scrapping/data.json") as file :
+        all_data = json.load(file)
+        return all_data
+
+def load_first_15_words () :
+    with open ("web-scrapping/first_15_word.json") as file :
+        all_first_15_words = json.load(file)
+        return all_first_15_words
+
+def save_links (all_links) :
+    with open ("web-scrapping/links.json", "w") as file :
+        json.dump(all_links,file)
+
+def save_titles (all_data) :
+    with open ("web-scrapping/titles.json", "w") as file :
+        json.dump(all_data,file)
+
+def save_data (all_titles) :
+    with open ("web-scrapping/data.json", "w") as file :
+        json.dump(all_titles,file)
+
+def save_first_15_words (all_first_15_words) :
+    with open ("web-scrapping/first_15_word.json", "w") as file :
+        json.dump(all_first_15_words,file)
+def retrieve_information(query):
+
+    all_links = load_links()
+
+    all_titles = load_titles()
+
+    all_first_15_words = load_first_15_words()
+
+    # Load preprocess numpy array
+    cleared_sentence_list = np.load("cleared_sentence.npy")
+    # Process query
+    query = [" ".join(process_sentence(query))]
+
+    # Init vectorizer
+    vectorizer = TfidfVectorizer()
+    # print(cleared_sentence_list)
+    # print(cleared_sentence_list[0])
+    # x = vectorize(cleared_sentence_list)
+    # x = np.asarray(x)
+    # print(x)
+    # print("---")
+    # x = np.transpose(x.T.toarray())
+    # print(type(x))
+
+    # Vectorize query
+    q_vec = vectorize(query)
+    # print(vectorizer.transform(query).toarray())
+    # print("------------------------------------------------------")
+    # print(query)
+    # print(vectorizer.transform(query))
+    # print(q_vec)
+
+    similarity = {}
+
+    # Get cosine similarity
+    # for i in range(len(cleared_sentence_list)):
+    #     if (np.linalg.norm(x[i]) * np.linalg.norm(q_vec)) == 0:
+    #         similarity[i] = 0
+    #     else:
+    #         similarity[i] = np.dot(x[i], q_vec) / (np.linalg.norm(x[i]) * np.linalg.norm(q_vec))
+    # Sort similarity
+    similarity_sorted = sorted(similarity.items(), key=lambda x: x[1], reverse=True)
+    ranks = []
+
+    # Get ranks
+    for indeks, sim in similarity_sorted:
+        if sim != 0.0 :
+            data = {
+                "title" : all_titles[indeks],
+                "links" : all_links[indeks],
+                "first_15_words" : all_first_15_words[indeks],
+                "similarity" : sim,
+            }
+            ranks.append(data)
+    return ranks
+
+def upload_file (file,filename) :
+    if filename.lower().endswith(".pdf"):
+        print(file)
+        print("sampai sini")
+        result = pdftotext.PDF(file)
+        print(result)
+        result = "\n\n".join(result)
+        first_15_words = get_first_15_words(result)
+
+
+        all_links = load_links()
+
+        all_titles = load_titles()
+
+        all_data = load_data()
+
+        all_first_15_words = load_first_15_words()
+
+
+        print(baseURL + filename)
+        all_links.append(baseURL + filename)
+        all_titles.append(filename)
+        all_data.append(str(result))
+        all_first_15_words.append(first_15_words)
+
+
+        save_links(all_links)
+        save_titles(all_titles)
+        save_data(all_data)
+        save_first_15_words(all_first_15_words)
+
+        preprocess()
+    elif filename.lower().endswith(".txt"):
+        file_ = file.read()
+        print(file_)
+        first_15_words = get_first_15_words(str(file_))
+
+        all_links = load_links()
+
+        all_titles = load_titles()
+
+        all_data = load_data()
+
+        all_first_15_words = load_first_15_words()
+
+
+        all_links.append(baseURL + filename)
+        all_titles.append(filename)
+        all_data.append(str(file_))
+        all_first_15_words.append(first_15_words)
+
+
+        save_links(all_links)
+        save_titles(all_titles)
+        save_data(all_data)
+        save_first_15_words(all_first_15_words)
+
+        preprocess()
+    elif filename.lower().endswith(".doc"):
+        pass
+    elif filename.lower().endswith(".html"):
+        soup = BeautifulSoup(file,"html.parser")
+        text = soup.text
+        first_15_words = get_first_15_words(file)
+
+        all_links = load_links()
+
+        all_titles = load_titles()
+
+        all_data = load_data()
+
+        all_first_15_words = load_first_15_words()
+
+        all_links.append(baseURL + filename)
+        all_titles.append(filename)
+        all_data.append(str(text))
+        all_first_15_words.append(first_15_words)
+
+        save_links(all_links)
+        save_titles(all_titles)
+        save_data(all_data)
+        save_first_15_words(all_first_15_words)
+
+        preprocess()
+
+    #
+
+    # preprocess()
+
+if __name__ == "__main__" :
+
+    print(retrieve_information("fullmet tes"))
---
+++ server/vectorize.py
@@ -0,0 +1,157 @@
+import numpy as np
+import pandas as pd
+import math
+from sklearn.feature_extraction.text import TfidfVectorizer
+from collections import Counter
+
+def decimalize(arr, total):
+    for i in range (len(arr)):
+        arr[i] = arr[i]/total
+    return arr
+
+# def concat(arr_before, arr_result, i):
+#     if i == 0:
+#         arr_result[0] = arr_before
+#     else:
+        
+
+def vectorize1(arr):
+    length = len(arr)
+    for i in range (length):
+        sentence = arr[i]
+        words = sentence.split()
+        unique = set(words)
+        arr_str = [0 for i in range (len(unique))]
+        arr_count = [0 for i in range (len(unique))]
+        before = 0
+        for j in range (len(words)):
+            if words[j] not in arr_str:
+                arr_str[before] = words[j]
+                arr_count[before] += 1
+                before += 1
+            else:
+                where = arr_str.index(words[j])
+                arr_count[where] += 1
+        print(arr_count)
+        new_arr_count = decimalize(arr_count, len(words))
+        if i == 0:
+            overall = [[]]
+            overall[0] = new_arr_count
+        # else:
+        #     print(overall)
+        #     arr_count = [arr_count]
+        #     np.concatenate((overall, arr_count), axis = 0)
+        print("-------")
+        print(overall, i)
+        arr_str.clear()
+        arr_count.clear()
+
+arr = ['ya hem iya ya hem meong', 'lalalala kamu siapa sih']
+# vectorize(arr)
+
+def vectorize2(arr):
+    # print(arr)
+    total_docs = len(arr)
+    
+
+    # To remove duplicate words
+    bag_words = []
+    for i in range (total_docs):
+        sentence = arr[i]
+        words = sentence.split()
+        bag_words.append(words)
+        if i == 0:
+            unique_words = words
+        else:
+            unique_words = set(unique_words).union(set(words))
+    
+    # print(bag_words)
+    # print(unique_words)
+
+    # Search for occurence every words from each documents
+    words_list = list(unique_words)
+    count_words = [[0 for j in range (len(words_list))] for i in range (total_docs)]
+    
+    for i in range (total_docs):
+        for j in range (len(words_list)):
+            if j < len(bag_words[i]):
+                if bag_words[i][j] in words_list:
+                    idx = words_list.index(bag_words[i][j])
+                    count_words[i][idx] += 1
+    # print(count_words)
+    
+    # Compute TF
+    tf = [[0 for j in range (len(words_list))] for i in range (total_docs)]
+    for i in range (total_docs):
+        bag_words_count = len(bag_words[i])
+        for j in range (len(count_words[i])):
+            tf[i][j] = count_words[i][j] / bag_words_count
+
+    # print(tf)
+    print(total_docs)
+    # Compute IDF
+    idf = [0 for i in range (len(words_list))]
+    for i in range (total_docs):
+        for j in range (len(words_list)):
+            if count_words[i][j] != 0:
+                idf[j] += 1
+
+    for j in range (len(words_list)):
+        print(idf[j])
+        idf[j] = math.log(total_docs / idf[j])
+
+    # print(idf)
+    # Compute TF-IDF
+    tfidf = [[0 for j in range (len(words_list))] for i in range (total_docs)]
+    for i in range (total_docs):
+        for j in range (len(words_list)):
+            tfidf[i][j] = tf[i][j] * idf[j]
+    # x = np.transpose(tfidf.toarray())
+    print(tfidf)
+    return tfidf
+
+def doc_freq(word):
+    c = 0
+    try:
+        c = DF[word]
+    except:
+        pass
+    return c
+
+def vectorize(arr):
+    total_docs = len(arr)
+
+    # Convert list to dict
+    words_dict = {}
+    for i in range (total_docs):
+        sentence = arr[i]
+        words = sentence.split()
+        for x in words:
+            try :
+                words_dict[x].add(i)
+            except:
+                words_dict[x] = {i}
+
+    # Calculate occurence of every words
+    for i in words_dict:
+        words_dict[i] = len(words_dict[i])
+
+    # Compute TF-IDF
+    doc = 0
+    tf_idf= {}
+    N = total_docs
+    for i in range(N):    
+        tokens = arr[i]
+        counter = Counter(tokens)
+        words_count = len(tokens)
+        for token in np.unique(tokens): 
+            tf = counter[token]/words_count
+            df = doc_freq(token)
+            idf = np.log((N+1)/(df+1)) 
+            tf_idf[doc, token] = tf*idf
+        doc += 1
+    print(tf_idf)
+    
+documentA = ['the man went out for a walk walk', 'the children sat around the fire']
+
+vectorize2(documentA)
