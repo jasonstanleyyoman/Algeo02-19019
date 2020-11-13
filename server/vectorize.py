6f01b722a9c2d960b89f5ecca80127b7d2f37c73
cyn-rus <cyn.rusadi@gmail.com>
2020-11-13 15:10:28 +0700

    ubah jadi fungsi

--- server/vectorize.py
+++ server/vectorize.py
@@ -1,5 +1,59 @@
 import math
 
+# Compute TF
+def ratio(vector, total, dict_length, tf):
+    for i in range (dict_length):
+        tf[i] = vector[i] / total
+    return tf
+
+def TF(document, query, dict_length, total_docs):
+    tf_document = [[0 for j in range (dict_length)] for i in range (total_docs)]
+    tf_query = [0 for i in range (dict_length)]
+    for i in range (total_docs+1):
+        if i < total_docs:
+            total = sum(document[i])
+            tf_document[i] = ratio(document[i], total, dict_length, tf_document[i])
+        else:
+            total = sum(query)
+            if total != 0:
+                tf_query = ratio(query, total, dict_length, tf_query)
+    return tf_document, tf_query
+# End of Compute TF
+
+# Compute IDF
+def IDF(dict_length, total_docs, document, query):
+    idf = [0 for i in range (dict_length)]
+    idf_doc = idf
+    idf_que = idf
+    for i in range (total_docs):
+        for j in range (dict_length):
+            if document[i][j] != 0:
+                idf[j] +=1
+    
+    for i in range (dict_length):
+        idf_doc[i] = 1 + math.log(total_docs / idf[i])
+        if query[i] != 0:
+            idf_que[i] = idf_doc[i]
+    return idf_doc, idf_que
+# End of Compute IDF
+
+# Compute TF-IDF
+def TF_IDF(dict_length, total_docs, document, query):
+    tfidf_doc = [[0 for j in range (dict_length)] for i in range (total_docs)]
+    tfidf_que = [0 for i in range (dict_length)]
+
+    tf_doc, tf_que = TF(document, query, dict_length, total_docs)
+    idf_doc, idf_que = IDF(dict_length, total_docs, document, query)
+
+    for i in range (total_docs+1):
+        for j in range (dict_length):
+            if i < total_docs:
+                tfidf_doc[i][j] = tf_doc[i][j] * idf_doc[j]
+            else:
+                tfidf_que[j] = tf_que[j] * idf_que[j]
+    return tfidf_doc, tfidf_que
+# End of Compute TF-IDF
+
 # Compute TFIDF of document and query
 def vectorizer(arr,query) :
     # Listing of unique word
@@ -30,51 +84,8 @@ def vectorizer(arr,query) :
 
     list_kata = list(list_kata)
 
-    # Compute TF (ratio of every words)
-    tf_document = [[0 for j in range (len(list_kata))] for i in range (banyak_dokumen)]
-    tf_query = [0 for i in range (len(list_kata))]
-    # print(document_vector)
-    for i in range (banyak_dokumen+1):
-        if i < banyak_dokumen:
-            total = sum(document_vector[i])
-        else:
-            total = sum(query_vector)
-        for j in range (len(list_kata)):
-            if i < banyak_dokumen:
-                tf_document[i][j] = document_vector[i][j] / total
-            elif total != 0:
-                tf_query[j] = query_vector[j] / total
-    # End of Compute TF (ratio of every words)
-
-    # Compute IDF
-    idf = [0 for i in range (len(list_kata))]
-    idf_document = [0 for i in range (len(list_kata))]
-    idf_query = [0 for i in range (len(list_kata))]
-    for i in range (banyak_dokumen):
-        for j in range (len(list_kata)):
-            if document_vector[i][j] != 0:
-                idf[j] += 1
-
-    for i in range (len(list_kata)):
-        try:
-            idf_document[i] = 1 + math.log(banyak_dokumen / idf[i])
-        except e:
-            print(e)
-        if query_vector[i] != 0:
-            idf_query[i] = idf_document[i]
-    # End of Compute IDF
-
     # Compute TF-IDF
-    tfidf_documents = [[0 for j in range (len(list_kata))] for i in range (banyak_dokumen)]
-    tfidf_query = [0 for i in range (len(list_kata))]
-    for i in range (banyak_dokumen+1):
-        for j in range (len(list_kata)):
-            if i < banyak_dokumen:
-                tfidf_documents[i][j] = tf_document[i][j] * idf_document[j]
-            else:
-                tfidf_query[j] = tf_query[j] * idf_query[j]
-
-    # End of Compute TF-IDF
+    tfidf_documents, tfidf_query = TF_IDF(len(list_kata), banyak_dokumen, document_vector, query_vector)
 
     return (tfidf_documents,tfidf_query, list(list_kata))
 # Compute Dot
@@ -91,7 +102,7 @@ def norm (vector) :
 
 if __name__ == "__main__" :
     documentA = ['The game of life is a game of everlasting learning', 'The unexamined life is not worth living', 'Never stop learning']
-    query = ["unexamined"]
+    query = ["hai"]
     tfidf_documents, tfidf_query, list_kata = vectorizer(documentA, query)
     print(tfidf_documents)
     print("---")
